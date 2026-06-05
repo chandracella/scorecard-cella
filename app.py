85d311df0b661a998bd06a32b1800409ee416e53
chandracella <chandra.cella@gmail.com>
2026-06-05 14:02:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Scorecard Automation", layout="wide")
-
-st.title("📊 Weekly Scorecard Automation")
+st.title("Weekly Scorecard Automation")
 
 uploaded_file = st.file_uploader(
     "Upload Excel",
@@ -12,42 +10,12 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file:
 
-    df = pd.read_excel(
+    raw = pd.read_excel(
         uploaded_file,
         sheet_name="Cella",
-        header=0
+        header=None
     )
 
-    # Ambil semua kolom week
-    week_cols = []
+    st.write("Raw Sheet Cella")
 
-    for col in df.columns:
-        if "-" in str(col):
-            week_cols.append(col)
-
-    st.success("Excel loaded successfully")
-
-    selected_week = st.selectbox(
-        "Select Week",
-        week_cols
-    )
-
-    st.write("Selected Week:", selected_week)
-
-    st.subheader("Preview Data")
-
-    preview_cols = [
-        "AGENT LIST",
-        "CODE",
-        "STATION",
-        selected_week
-    ]
-
-    preview_cols = [
-        c for c in preview_cols
-        if c in df.columns
-    ]
-
-    st.dataframe(
-        df[preview_cols].head(30)
-    )
+    st.dataframe(raw.head(15))
