4f4eb9281a773e49c473502dbbfcbb33e1be94f8
chandracella <chandra.cella@gmail.com>
2026-06-05 13:48:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.set_page_config(page_title="Scorecard Cella")
+st.set_page_config(page_title="Scorecard Cella", layout="wide")
 
 st.title("📊 Weekly Scorecard Automation")
 
@@ -10,4 +11,22 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file:
+
+    xls = pd.ExcelFile(uploaded_file)
+
     st.success("Excel uploaded successfully")
+
+    st.write("Sheets found:")
+    st.write(xls.sheet_names)
+
+    if "Cella" in xls.sheet_names:
+        df_cella = pd.read_excel(uploaded_file, sheet_name="Cella")
+
+        st.subheader("Preview - Sheet Cella")
+        st.dataframe(df_cella.head(20))
+
+    if "MASTER" in xls.sheet_names:
+        df_master = pd.read_excel(uploaded_file, sheet_name="MASTER")
+
+        st.subheader("Preview - Sheet MASTER")
+        st.dataframe(df_master.head(20))
